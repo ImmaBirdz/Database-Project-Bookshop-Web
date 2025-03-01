9b32db3e76b2263e6c71798321569a975f0364e4
ImmaBirdz <nakeza1375@hotmail.com>
2025-03-01 14:49:55 +0700

    Create book table

---
+++ app/Models/Book.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Book extends Model
+{
+    // it contains book_id, book_title, book_category, book_price, book_status
+    protected $fillable = [
+        'book_id',
+        'book_title',
+        'book_category',
+        'book_price',
+        'book_status',
+    ];
+
+    protected $casts = [
+        'book_status' => 'boolean',
+    ];
+
+    protected $attributes = [
+        'book_status' => true,
+    ];
+}
