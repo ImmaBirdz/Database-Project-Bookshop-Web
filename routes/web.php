1eb1c9672158f3abc80a7a7a407cb8926081fef1
kzy47 <151009725+kzy47@users.noreply.github.com>
2025-03-05 12:50:50 +0700

    (side B L ...but i still dont know how to add icon  from figma)

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,11 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+
+//book route
+Route::get('/book', function () {
+    return view('book');
+});
+
+
 require __DIR__.'/auth.php';
