19283fc3bfa3bdc8946a9db2fb78ab0d3d1e7d7b
ImmaBirdz <nakeza1375@hotmail.com>
2025-03-10 00:19:03 +0700

    Update routes on book, explore, and add wishlist, cart on delete

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,10 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
 use App\Http\Controllers\WishListController;
 use App\Http\Controllers\CartController;
 use App\Http\Controllers\ExploreController;
-use App\Http\Controllers\BookController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -19,10 +19,16 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::resource('/explore', ExploreController::class); // explore route
-    Route::resource('/book', BookController::class); // book route
-    Route::resource('/wishlist', WishListController::class); // wishlist route
-    Route::resource('/cart', CartController::class); // cart route
+    Route::resource('book', BookController::class); // Book Routes
+    Route::resource('wishlist', WishListController::class); // Wishlist Routes
+    Route::delete('/wishlist/{id}', [WishlistController::class, 'remove'])->name('wishlist.remove'); // Route to remove item from wishlist
+    Route::resource('cart', CartController::class); // Wishlist Routes
+    Route::delete('/cart/{id}', [CartController::class, 'remove'])->name('cart.remove'); // Route to remove item from cart
+});
+
+// Explore Routes
+Route::get('/explore', function () {
+    return view('explore');
 });
 
 require __DIR__.'/auth.php';
