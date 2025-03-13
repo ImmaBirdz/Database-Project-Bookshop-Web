185d19cffb73a3b75611141fe2c885404344dfa4
ImmaBirdz <nakeza1375@hotmail.com>
2025-03-14 06:07:04 +0700

    Edit Seeder
    format, add url to book_cover, etc.

--- database/seeders/AuthorSeeder.php
+++ database/seeders/AuthorSeeder.php
@@ -18,34 +18,50 @@ class AuthorSeeder extends Seeder
             [
                 'author_id' => 1,
                 'author_name' => 'Fyodor Dostoevsky',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
             ],
             [
                 'author_id' => 2,
                 'author_name' => 'Albert Camus',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
             ],
             [
                 'author_id' => 3,
                 'author_name' => 'Hanya Yanagihara',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
             ],
             [
                 'author_id' => 4,
                 'author_name' => 'George Orwell',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
             ],
             [
                 'author_id' => 5,
                 'author_name' => 'Franz Kafka',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
             ],
             [
                 'author_id' => 6,
                 'author_name' => 'E.M.Forster',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
             ],
             [
                 'author_id' => 7,
                 'author_name' => 'Margaret Atwood',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
             ],
             [
                 'author_id' => 8,
                 'author_name' => 'Louisa May Alcott',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
             ],
         ]);
     }
