b14738999664712b3b2d0665d1228ada550f069b
ImmaBirdz <nakeza1375@hotmail.com>
2025-03-14 19:11:27 +0700

    Minor change in cart table

--- database/migrations/2025_03_05_115232_create_carts_table.php
+++ database/migrations/2025_03_05_115232_create_carts_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('carts', function (Blueprint $table) {
-            $table->string('cart_id')->unique();
+            $table->id('cart_id')->unique();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('book_id');
             $table->foreign('book_id')->references('book_id')->on('books')->onDelete('cascade');
