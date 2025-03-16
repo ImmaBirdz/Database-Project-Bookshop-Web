55f4aa0865396c0f0c1d924ab8e295bfd054129e
ImmaBirdz <nakeza1375@hotmail.com>
2025-03-17 06:02:37 +0700

    Create collection table

---
+++ app/Models/Collection.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Collection extends Model
+{
+    protected $primaryKey = 'collection_id';
+    protected $fillable = [
+        'collection_id',
+        'user_id',
+        'book_id',
+        'quantity'
+    ];
+}
+
+public function users(){
+    return $this->belongsTo(User::class);
+}
+
+public function books(){
+    return $this->hasMany(Book::class);
+}
---
+++ database/migrations/2025_03_16_222244_create_collections_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('collections', function (Blueprint $table) {
+            $table->string('collection_id')->unique();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('book_id');
+            $table->foreign('book_id')->references('book_id')->on('books')->onDelete('cascade');
+            $table->integer('quantity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('collections');
+    }
+};
