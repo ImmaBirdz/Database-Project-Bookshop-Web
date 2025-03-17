446bdb5515be9c4a56d8088c96dbe08ecf766614
ImmaBirdz <nakeza1375@hotmail.com>
2025-03-17 09:14:21 +0700

    Justify capitalize of wishlist

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController;
-use App\Http\Controllers\WishListController;
+use App\Http\Controllers\WishlistController;
 use App\Http\Controllers\CartController;
 use App\Http\Controllers\ExploreController;
 use App\Http\Controllers\BrowseController;
