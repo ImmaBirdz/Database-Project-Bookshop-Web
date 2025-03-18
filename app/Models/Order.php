a329bd57fb397692cb54c6663644a93f9d2ff58a
ImmaBirdz <nakeza1375@hotmail.com>
2025-03-18 08:20:21 +0700

    Fix retrieving order_id as a string

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     protected $primaryKey = 'order_id';
+    protected $keyType = 'string';
     protected $fillable = [
         'order_id',
         'user_id',
